f4c87dafe1a5b7e9aca6112d9481547bd962a061
Victoria Trac <vtrac1@ualberta.ca>
2024-03-08 13:54:44 -0700

    hasCity written

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -68,7 +68,7 @@ public class CustomList extends ArrayAdapter<City> {
      */
     public boolean hasCity(City city){
 
-//        return cities.contains(city);
+        return cities.contains(city);
     }
 
 }
